4e9e3c77a75705e808f2e86cc5c960bc7131bac2
Mauricio Silveira <mauricio.machado.silveira@gmail.com>
2018-02-20 00:59:06 -0500

    Create functions to read content from database - Created functions to bring content of sections, categories and items - Display content of Life page

---
+++ includes/read.php
@@ -0,0 +1,48 @@
+<?php
+
+  function getSectionContent($section) {
+    include('connect.php');
+
+    $querySection = "SELECT * FROM section WHERE name = '{$section}'";
+    $result = mysqli_query($link, $querySection);
+    if($result){
+			return $result;
+		}else{
+			$error = "There was a problem accessing this information.";
+			return $error;
+		}
+
+    mysqli_close($link);
+  }
+
+  function getCategories($sectionId) {
+    include('connect.php');
+
+    $queryCategory = "SELECT cat.* FROM section sec, category cat, section_category seccat WHERE sec.id = {$sectionId} AND sec.id = seccat.section_id AND seccat.category_id = cat.id";
+    $result = mysqli_query($link, $queryCategory);
+    if($result){
+			return $result;
+		}else{
+			$error = "There was a problem accessing this information.";
+			return $error;
+		}
+
+    mysqli_close($link);
+  }
+
+  function getItems($categoryId) {
+    include('connect.php');
+
+    $itemQuery = "SELECT it.* FROM category cat, item it, category_item catit WHERE cat.id = {$categoryId} AND cat.id = catit.category_id AND catit.item_id = it.id";
+    $result = mysqli_query($link, $itemQuery);
+    if($result){
+			return $result;
+		}else{
+			$error = "There was a problem accessing this information.";
+			return $error;
+		}
+
+    mysqli_close($link);
+  }
+
+ ?>
--- life.php
+++ life.php
@@ -1,3 +1,26 @@
+<?php
+  require_once('includes/config.php');
+
+  $sectionData = getSectionContent('life');
+  if(!is_string($sectionData)) {
+    $section = mysqli_fetch_array($sectionData);
+    $categoriesData = getCategories($section['id']);
+    // if(!is_string($categoriesData)){
+    //   $categories = array();
+    //   while ($category = mysqli_fetch_assoc($categoriesData)) {
+    //     $categories[] = $category;
+    //   }
+    // }
+    // else {
+    //   redirect_to('error.php');
+    // }
+  }
+  else {
+    redirect_to('error.php');
+  }
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,18 +38,85 @@
       <?php include('includes/partials/header.html'); ?>
     </div>
 
+    <?php if (!empty($message)){
+      echo "<p class=\"error-message\">".$message."</p>";
+    } ?>
 
     <main>
-      <h1>Some text here</h1>
-      <p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum felis nulla, pellentesque eu pretium id, accumsan sit amet felis. Duis ornare sem non mauris rhoncus convallis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris hendrerit ligula vitae dui euismod, ac consectetur est pulvinar. Nunc in mauris pulvinar, volutpat nibh in, tincidunt arcu. Proin fermentum consequat dolor, nec commodo nunc varius suscipit. Mauris gravida augue ac euismod facilisis. Sed et maximus lectus. Integer ac ex fermentum, commodo dolor vel, efficitur arcu. Donec ut sem tincidunt, gravida elit at, interdum mi. Cras iaculis, dui mollis mattis luctus, mauris nunc ornare arcu, interdum vehicula metus ante non felis. Nulla et tempus nisi. Phasellus id sagittis dolor. Nunc ultricies, ipsum mollis tincidunt aliquam, leo sem placerat lacus, et ullamcorper metus eros id velit.
-</p>
-<p>
-Quisque urna nibh, feugiat vitae purus interdum, porta viverra nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus feugiat eget orci et tincidunt. Morbi eros urna, accumsan non consequat et, interdum non lacus. Mauris condimentum sem et ultrices gravida. Sed ultricies vitae massa id interdum. Aliquam gravida dui vel ligula elementum, ut feugiat risus fermentum. Phasellus a magna commodo, egestas diam non, pellentesque purus. Nullam eu diam sit amet nibh dapibus porta nec in diam. Ut porta semper risus eu euismod. Etiam justo turpis, sollicitudin sit amet nisi ut, euismod laoreet eros. Ut tristique eu velit vel tempus. Cras nec sapien massa. Praesent fringilla volutpat elit, vel maximus tellus faucibus vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque massa quis risus semper, sit amet sollicitudin ipsum hendrerit.
-</p>
-<p>
-Nullam ultrices ac mauris posuere feugiat. Integer ullamcorper fringilla commodo. Donec interdum, massa vitae mollis sagittis, dolor orci tincidunt nunc, vel ultrices urna magna vel orci. Curabitur fermentum sed urna eget porta. Pellentesque id sodales elit. Nunc sit amet varius lorem, nec eleifend magna. Aenean laoreet, tellus nec hendrerit fermentum, nunc nunc molestie erat, sit amet facilisis dui lacus vitae lorem. Pellentesque odio sapien, placerat id orci vitae, interdum condimentum libero. Integer euismod neque nec leo euismod bibendum. Fusce augue magna, ultricies id egestas a, accumsan sed libero. Aenean sit amet odio risus. Suspendisse sed dui at nisi aliquam fermentum quis eu est. Mauris mollis tortor eget volutpat scelerisque. Ut pulvinar urna sed congue ultrices. Nulla facilisi. Aliquam mattis eget ex eget efficitur.
-</p>
+      <div class="section-title">
+        <?php
+          if($section) {
+            echo "<img src=\"images/{$section['icon']}_small.png\" alt=\"Some photo\">
+              <h1>{$section['title']}</h1>";
+          }
+          else {
+            echo "<p>{$sectionData}</p>";
+          }
+         ?>
+      </div>
+
+      <section class="main-content">
+        <?php
+          if($section) {
+            echo "<p>{$section['description']}</p>";
+          }
+          else {
+            echo "<p>{$sectionData}</p>";
+          }
+        ?>
+      </section>
+
+      <?php
+        if(!is_string($categoriesData)){
+          while ($category = mysqli_fetch_assoc($categoriesData)) {
+      ?>
+        <section class="category">
+          <?php echo "<button type=\"button\" name=\"{$category['title']}\">{$category['title']}</button>" ?>
+          <h2><?php echo $category['title'] ?></h2>
+          <p><?php echo $category['short_desc'] ?></p>
+          <p><?php echo $category['description'] ?></p>
+          <?php
+            $itemData = getItems($category['id']);
+            if(!is_string($itemData)){
+              while ($item = mysqli_fetch_assoc($itemData)) {
+                if($item['description']) {
+          ?>
+                <section class="item">
+                  <h3><?php echo $item['title']; ?></h3>
+                  <p><?php echo $item['description']; ?></p>
+                  <p>WHERE: <?php echo $item['address']; ?></p>
+                  <p>CONTACT: <?php echo $item['phone']; ?></p>
+                  <p>HOURS: <time><?php echo $item['hours']; ?></time></p>
+                </section>
+          <?php
+                }
+                else {
+          ?>
+                <section class="item nocontent">
+                  <h3><?php echo $item['title']; ?></h3>
+                </section>
+          <?php
+                }
+              }
+            }
+            else {
+              redirect_to('error.php');
+            }
+          ?>
+        </section>
+      <?php
+          }
+        }
+        else {
+          // redirect_to('error.php');
+          echo "error";
+        }
+      ?>
+
+
+      <section id="parks">
+
+      </section>
     </main>
 
 
