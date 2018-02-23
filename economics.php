65a10984cbddb8824ad06d24cd49215f9ab16129
Mauricio Silveira <mauricio.machado.silveira@gmail.com>
2018-02-23 18:59:44 -0500

    Fix some style bugs - Other parks images on Life Page - Footer on Work page - Included general layout to Work and Economics pages - Do not show buttons "close section" to tablet and desktop screens

--- economics.php
+++ economics.php
@@ -31,37 +31,46 @@
 
 
     <main>
-      <div class="section-title">
-        <?php
-          if($section) {
-            echo "<img class=\"section-icon\" src=\"images/{$section['icon']}_small.png\" alt=\"Some photo\">
-              <h1 class=\"section-title\">{$section['title']}</h1>";
-          }
-          else {
-            echo "<p>{$sectionData}</p>";
-          }
-         ?>
-      </div>
+      <section>
+        <div class="section-header">
+          <?php
+            if($section) {
+              echo "<img class=\"section-icon\" src=\"images/{$section['icon']}.png\" alt=\"Some photo\">
+                <h1 class=\"section-title\">{$section['title']}</h1>";
+            }
+            else {
+              echo "<p>{$sectionData}</p>";
+            }
+           ?>
+        </div>
 
-      <section class="section-desc">
-        <?php
-          if($section) {
-            echo "<p class=\"section-desc\">{$section['description']}</p>";
-          }
-          else {
-            echo "<p>{$sectionData}</p>";
-          }
-        ?>
+        <div class="section-info">
+          <?php
+            if($section) {
+              echo "<p class=\"section-desc\">{$section['description']}</p>";
+            }
+            else {
+              echo "<p>{$sectionData}</p>";
+            }
+          ?>
+        </div>
       </section>
 
       <?php
         if(!is_string($categoriesData)) {
           while ($category = mysqli_fetch_assoc($categoriesData)) {
       ?>
-        <?php echo "<button class=\"category-button\" type=\"button\" name=\"{$category['name']}\">{$category['title']}</button>" ?>
+        <?php echo "<button class=\"open-category-btn\" type=\"button\" name=\"{$category['name']}\">{$category['title']}</button>" ?>
         <section id="<?php echo $category['name']; ?>" class="category">
-          <h2 class="category-title"><?php echo $category['title']; ?></h2>
-          <p class="category-short"><?php echo $category['short_desc']; ?></p>
+          <div class="category-header">
+            <div class="photo-wrapper">
+              <img src="images/london2_002-5857.jpg" alt="Photo">
+            </div>
+            <div class="category-info">
+              <h2 class="category-title"><?php echo $category['title']; ?></h2>
+              <p class="category-short"><?php echo $category['short_desc']; ?></p>
+            </div>
+          </div>
           <?php
             if($category['name'] === 'starting') {
               echo "<p>Quick tips from local employers</p>";
@@ -81,6 +90,7 @@
                 </section>
           <?php
               }
+            echo "<button class=\"close-category-btn\" type=\"button\" name=\"{$category['name']}\"><i class=\"ion-android-close\"></i>Close section</button>";
             }
             else {
               redirect_to('error.php');
